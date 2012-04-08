26b51e600fc0ccfa4fc1aad8bb2af9bc3680a7b9
Robin Cornelius <robin.cornelius@hirst-magnetics.com>
2012-04-08 09:09:28 +0100

    Dont show the server connect window on startup if it is not configured

--- src/mumble/main.cpp
+++ src/mumble/main.cpp
@@ -448,7 +448,7 @@ int main(int argc, char **argv) {
 		qApp->postEvent(g.mw, oue);
 #endif
 	} else {
-		g.mw->on_qaServerConnect_triggered(true);
+		//g.mw->on_qaServerConnect_triggered(true);
 	}
 
 	if (! g.bQuit)
